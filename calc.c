0db6d36d33fc46447213df777a02121a278cdb59
theAester <hiradcodes@yahoo.com>
2022-12-06 01:47:36 +0330

    tidy up

--- calc.c
+++ calc.c
@@ -4,30 +4,32 @@
 #include <math.h>
 
 #include "numeric.h"
+#include "poly/poly.h"
+#include "solv/solv.h"
 
 // pass func
-double func(double val, struct cb_args p){
-	return npoly_eval((struct npoly*)(p.data), val);
+double func(double val, void* p){
+	return npoly_eval((struct npoly*)p, val);
 }
 
 //derivative
-double func_prime(double val, struct cb_args p){
-	return npoly_eval(((struct npoly*)(p.data))+1, val);
+double func_prime(double val, void* p){
+	return npoly_eval(((struct npoly*)p)+1, val);
 }
 
 int main(void){
 	struct npoly p[2];
-	npoly_fill(p, 5, 
-					0.22, -3.27, -2.74, 2.81, -3.36, 2.0);
+	npoly_fill(p, 5, 0.22, -3.27, -2.74, 2.81, -3.36, 2.0);
+	//            ^    ^      ^     ^      ^      ^    ^
+	//        degree  x^5    x^4   x^3    x^2    x^1  x^0
 
 	npoly_create(p+1, 4);
 	npoly_derive(p+1, p);
 
-	struct cb_args cba;
-	cba.len = sizeof(struct poly*); 
-	cba.data = p;
-	
 	struct nreport rep;
-	double sol = ncomp_newton_er(func, func_prime, cba, 3, 1e-8, &rep);
-	printf("%lf\n%lf %d\n", sol, rep.error, rep.iterations);
+	double sol = ncomp_newton_er(func, func_prime, (void*)p, 3, 1e-8, &rep);
+	printf("%.9lf\n%.9lf %d\n", sol, rep.error, rep.iterations);
+	npoly_free(p);
+	npoly_free(p+1);
+	return 0;
 }
